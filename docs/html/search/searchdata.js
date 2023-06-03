987933650573e6fb226d7e6c69cc95819adbfefe
Zomat <mateusz.zolisz@gmail.com>
2023-06-04 01:45:44 +0200

    created docs

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,24 @@
+var indexSectionsWithContent =
+{
+  0: "k",
+  1: "k",
+  2: "k",
+  3: "k"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions"
+};
+
